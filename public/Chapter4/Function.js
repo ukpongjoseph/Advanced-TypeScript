005015d132e5bf69cdda2fc7cee3f9e42c406863
ukpongjoseph <aniekanjoseph499@gmail.com>
2025-09-17 02:33:30 +0100

    Updated Chapter 4, understood Generators, Iterators, iterables, call signature and overloaded function

--- public/Chapter4/Function.js
+++ public/Chapter4/Function.js
@@ -81,4 +81,87 @@ greet3();
 // Call and apply works in the same way, except that apply works with array
 // The bind keyword store the value of the function call in another variable. This new variable is then invoked
 // GENERATOR FUNCTION
+// Generator function are special kind of functions that can pause execution and resume execution unlike normal functions that run from start to finish at once
+// Generator functions are spotted with the "yield" keyword, next() method during function call and asterik (*). They return a generator object like {value: "output of execution", done: "true/false"}. Generator functions use the yield keyword to yield values. They also yield the next value whan called via the next() method.
+// Generator functions pause execution and return a generator object when they encouter the yield keyword. They resume execution when they encounter the next() method.
+// To stop execution of generator functions, we use the return keyword and to return an error we use the throw keyword. Both return and throw stops the function execution
+// Generator functions do not return void, a values must be return
+function* test(a, b) {
+    yield a;
+    yield b;
+}
+const functionReturn = test("Atanda", "Bondu");
+console.log(functionReturn.next());
+console.log(functionReturn.next());
+console.log(functionReturn.next());
+// The Generator function above takes two parameters and yield them one by one. When the function is initially invoked, the first yield is invoked and paused until when the second function call before the second yield is return. The third function call returns a generator object of yield to be undefined (because we arenot yielding or returning anything) and done to be true
+// Generator functions are used to return a bunch of value sequentially, handle large data piece by piece
+// ITERATORS AND ITERABLES
+// An iterator is an object with the next() method. To make it simpler, remember that arrays, objects and functions are objects (with functions as first class object). In a more simpler term, an iterator is an object (most likely a function ) with the next method. This iterator is able to iterate over an iterable (moving from one element or value in the iterable to another via the next() method and then returning a value). Iterators moves/iterates over an iterable and then return a value, most likely an object of the type, {value: return-value, done:true/false}.
+// An iterable on the other hand is any object or collection of data (array, string and so on) that has a [Symbol.iterator] method/ property. This [Symbol.iterator] property returns an iteraror that is able to iterate over an iterable and then return a value. Some types like arrays and strings have default implementation of an iterator and that is why it is easy to iterate over them. The compiler checks for their [Symbol.iterator] property, if present, methods like for loops and the likes are permitted. Not all types have a default iterator property!!!! and for thses types, to itearate over them, we need to create a custom [System.iterator] property so as to be able to iterate over them.
+// How do we create a custom iterable?.....It is shown below
+// Remember an iterable is an object, it has a [Symbol.iterator] property. The [Symbol.iterator] property returns an iterator function/method/object. This method/function/object return an Object with a next() method. The next method returns a value (an object with value property and done property) 
+const iterable = {
+    name: "Josiah",
+    [Symbol.iterator]: () => {
+        let items = [1, 2, 3, 4, 5, 6, 7];
+        let counter = 0;
+        return {
+            next() {
+                return {
+                    value: items[counter++],
+                    done: counter >= items.length ? true : false
+                };
+            }
+        };
+    }
+};
+// The above object is an iterable. It has a custom [Symbol.iterator], a function that returns an iterator that iterates over an hard coded array (items). This idea of making an iterable makes objects behave like an array. Example is shown below. The Object below (countDown) will be made to behave like an array only using its property counter
+const countDown = {
+    counter: 5,
+    [Symbol.iterator]() {
+        let number = this.counter;
+        return {
+            next() {
+                return {
+                    value: number--,
+                    done: number < 0 ? true : false
+                };
+            }
+        };
+    }
+};
+for (let num of countDown) {
+    // The logs to the console from this loop will be 5,4,3,2,1
+    console.log(num);
+}
+//  FUNCTION CALL SIGNATURE
+// Function call signature defines the structure of a function. These call signature can further be extraxted and bound to be type aliases. Thereby when a function is declared, its type will not be Function, rather it will be of the type aliase made from the call signature.
+// Say we have a function that recieves two parameters of type number, add then and returns a result of type number, the function signature will look like
+let sum3;
+sum3 = (a, b) => {
+    return a + b;
+};
+let addNumbers = (x, y) => {
+    return x + y;
+};
+let squareNumber = (a, b) => {
+    return a(b);
+};
+// Function implementation
+function add9(a, b) {
+    return a + b;
+}
+function sum6(a, b) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+    else if (typeof a === 'string' && typeof b === 'string') {
+        return a + b;
+    }
+    else {
+        throw new Error("Invalid type");
+    }
+}
+// From example 2, it can be seeen that the function implementation handles all cases (string or number)
 //# sourceMappingURL=Function.js.map
--- src/Chapter4/Function.ts
+++ src/Chapter4/Function.ts
@@ -87,6 +87,7 @@ const person = {
 }
 const details : string[] = ["Mariam"]
 function greet1(this:any) : void{
+
     console.log(`My name is ${this.name} and i am ${this.age} years old`)
 }
 function greet2(this:any) :void{
@@ -102,3 +103,111 @@ greet3()
 
 
 // GENERATOR FUNCTION
+// Generator function are special kind of functions that can pause execution and resume execution unlike normal functions that run from start to finish at once
+// Generator functions are spotted with the "yield" keyword, next() method during function call and asterik (*). They return a generator object like {value: "output of execution", done: "true/false"}. Generator functions use the yield keyword to yield values. They also yield the next value whan called via the next() method.
+// Generator functions pause execution and return a generator object when they encouter the yield keyword. They resume execution when they encounter the next() method.
+// To stop execution of generator functions, we use the return keyword and to return an error we use the throw keyword. Both return and throw stops the function execution
+// Generator functions do not return void, a values must be return
+function* test(a:string, b:string){
+    yield a
+    yield b
+}
+const functionReturn = test("Atanda", "Bondu")
+console.log(functionReturn.next())
+console.log(functionReturn.next())
+console.log(functionReturn.next())
+// The Generator function above takes two parameters and yield them one by one. When the function is initially invoked, the first yield is invoked and paused until when the second function call before the second yield is return. The third function call returns a generator object of yield to be undefined (because we arenot yielding or returning anything) and done to be true
+// Generator functions are used to return a bunch of value sequentially, handle large data piece by piece
+
+// ITERATORS AND ITERABLES
+// An iterator is an object with the next() method. To make it simpler, remember that arrays, objects and functions are objects (with functions as first class object). In a more simpler term, an iterator is an object (most likely a function ) with the next method. This iterator is able to iterate over an iterable (moving from one element or value in the iterable to another via the next() method and then returning a value). Iterators moves/iterates over an iterable and then return a value, most likely an object of the type, {value: return-value, done:true/false}.
+// An iterable on the other hand is any object or collection of data (array, string and so on) that has a [Symbol.iterator] method/ property. This [Symbol.iterator] property returns an iteraror that is able to iterate over an iterable and then return a value. Some types like arrays and strings have default implementation of an iterator and that is why it is easy to iterate over them. The compiler checks for their [Symbol.iterator] property, if present, methods like for loops and the likes are permitted. Not all types have a default iterator property!!!! and for thses types, to itearate over them, we need to create a custom [System.iterator] property so as to be able to iterate over them.
+// How do we create a custom iterable?.....It is shown below
+// Remember an iterable is an object, it has a [Symbol.iterator] property. The [Symbol.iterator] property returns an iterator function/method/object. This method/function/object return an Object with a next() method. The next method returns a value (an object with value property and done property) 
+
+const iterable = {
+    name : "Josiah",
+    [Symbol.iterator] : () => {
+        let items = [1,2,3,4,5,6,7]
+        let counter = 0
+        return {
+            next(){
+                return{
+                    value : items[counter++],
+                    done : counter>=items.length?true:false
+                }
+            }
+        }
+    }
+}
+// The above object is an iterable. It has a custom [Symbol.iterator], a function that returns an iterator that iterates over an hard coded array (items). This idea of making an iterable makes objects behave like an array. Example is shown below. The Object below (countDown) will be made to behave like an array only using its property counter
+const countDown = {
+    counter : 5,
+    [Symbol.iterator](this:any){
+        let number = this.counter
+        return {
+            next(){
+                return{
+                    value:number--,
+                    done: number<0?true:false
+                }
+            }
+        }
+    }
+}
+for(let num of countDown){
+    // The logs to the console from this loop will be 5,4,3,2,1
+    console.log(num)
+}
+
+//  FUNCTION CALL SIGNATURE
+// Function call signature defines the structure of a function. These call signature can further be extraxted and bound to be type aliases. Thereby when a function is declared, its type will not be Function, rather it will be of the type aliase made from the call signature.
+// Say we have a function that recieves two parameters of type number, add then and returns a result of type number, the function signature will look like
+let sum3 : (a:number, b:number) => number
+sum3 = (a,b) => {
+    return a + b
+}
+// The call signature can be bound to a type aliase
+type add = (a:number, b:number) => number
+let addNumbers:add = (x, y) => {
+    return x + y
+}
+// The function, addNumbers is of type add and it follows the call signature of add
+// It should be noted that default parameters do not go into callsignature 
+
+// OVERLOADED FUNCTION TYPE
+// Callback functions are functions that are passed as parameter/argument to another function
+type square = (a:(x:number)=>number, b:number) => number
+let squareNumber:square = (a,b) => {
+    return a(b)
+}
+// In the example above, a is a callback function that takes the parameter as its own, square it and return the result back to squareNumber
+
+//                  OVERLOADED FUNCTION
+// An overloaded function is a function that has multiple call signature, hence the term overloaded. Such function can be called with different input types and hence produce different output types depending on the inputs provided.
+// One thing to note about overloaded functions is that the function implementation should be the last of the call signature and it (simply the function declaration) must be compatible with all the multiple call signature. Say we define a function that recieves a defined set of parameters in different calls involving strings, numbers, string and numbers or boolean....the function implementation should be compatible with all these data type via the use of explicitly defining parameters as type, any, by using unions or by checking the type of the parameters before performing any operation
+// Example 1
+
+// Call signature (using type:any)
+function add9(a:number, b:number):number
+function add9(a:string, b:string):string
+// Function implementation
+function add9(a:any, b:any):any{
+    return a+b
+}
+// From example 1 , it can be seen that if the arguments to this function are either strings or numbers, the function would still be executed.
+
+// Example 2 (using union and type checking)
+function sum6(a:number, b:number):number
+function sum6(a:string, b:string):string
+
+function sum6(a:string|number, b:string|number):string|number{
+    if(typeof a === 'number' && typeof b === 'number'){
+        return a+b
+    }else if (typeof a === 'string' && typeof b === 'string'){
+        return a+b
+    }else{
+        throw new Error ("Invalid type")
+    }
+}
+// From example 2, it can be seeen that the function implementation handles all cases (string or number)
